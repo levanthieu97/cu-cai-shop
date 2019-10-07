0ef22a666618413e3fc90f5563bfccf42dd48f39
thieu_le <thieu.le@dounets.com>
2019-10-07 17:29:15 +0700

    Cu Cai Shop First Commit

---
+++ src/main/java/com/gaogao/cucaishop/CuCaiShopApplication.java
@@ -0,0 +1,13 @@
+package com.gaogao.cucaishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuCaiShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuCaiShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gaogao/cucaishop/CuCaiShopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gaogao.cucaishop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CuCaiShopApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
